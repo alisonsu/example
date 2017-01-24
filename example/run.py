be0e145d50ad5743df1f8df837cecea0c50dff68
Alison Su <alison.nc.su@gmail.com>
2017-01-24 11:20:44 -0800

    Updated run.py skeleton to correctly perform bubblesort and quicksort

--- example/run.py
+++ example/run.py
@@ -9,9 +9,17 @@ def run_stuff():
     This function is called in `__main__.py`
     """
     print("This is `run()` from ", __file__)
-
+    
     x = np.random.rand(10)
-    print("Unsorted input: ", x)
+    r = len(x)-1
+    p=0
+    
+    lst2=x[:]
 
-    print("Bubble sort output: ", bubblesort(x))
-    print("Quick sort output: ", quicksort(x))
+    print("Unsorted input: ", x)
+    
+    bubblesort(x)
+    quicksort(lst2,p,r)
+
+    print("Bubble sort output: ", x)
+    print("Quick sort output: ", lst2)
